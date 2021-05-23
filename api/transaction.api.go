e3aa1da1fd78adfd80a4398b54f4ce932df37231
Karoon Sillapapan <karoon@ananda.co.th>
2021-05-23 14:46:46 +0700

    Config transaction route

--- api/transaction.api.go
+++ api/transaction.api.go
@@ -1,7 +1,24 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SetupTransactionAPI(router *gin.Engine) {
+	transactionAPI := router.Group("/api/v2")
+	{
+		transactionAPI.GET("/transaction", getTransaction)
+		transactionAPI.POST("/transaction", createTransaction)
+	}
 
+}
+
+func getTransaction(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "Get transaction"})
+}
+
+func createTransaction(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "Create transaction"})
 }
