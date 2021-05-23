6392221432608e59ddb2581deed6e40ba223db60
Karoon Sillapapan <karoon@ananda.co.th>
2021-05-23 18:18:28 +0700

    Completed with login and generate token

--- api/authen.api.go
+++ api/authen.api.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/ksupdev/updev-go-ex-stock-api/db"
+	"github.com/ksupdev/updev-go-ex-stock-api/interceptor"
 	"github.com/ksupdev/updev-go-ex-stock-api/model"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -20,7 +21,21 @@ func SetupAuthenAPI(router *gin.Engine) {
 }
 
 func login(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"result": "login"})
+	var user model.User
+	if c.ShouldBind(&user) == nil {
+		var queryUser model.User
+		if err := db.GetDB().First(&queryUser, "username = ?", user.Username).Error; err != nil {
+			c.JSON(http.StatusOK, gin.H{"result": "nok", "error": err})
+		} else if !checkPasswordHash(user.Password, queryUser.Password) {
+			c.JSON(http.StatusOK, gin.H{"result": "nok", "error": "invalid password"})
+		} else {
+			token := interceptor.JwtSign(queryUser)
+			c.JSON(http.StatusOK, gin.H{"result": "OK", "token": token})
+		}
+	} else {
+		c.JSON(http.StatusOK, gin.H{"status": "Unable to bind data"})
+	}
+
 }
 
 func register(c *gin.Context) {
@@ -38,7 +53,7 @@ func register(c *gin.Context) {
 		if err := db.GetDB().Create(&user).Error; err != nil {
 			c.JSON(http.StatusOK, gin.H{"result": "nok", "error": err})
 		} else {
-			c.JSON(http.StatusOK, gin.H{"result": "OK", "data": user})
+			c.JSON(http.StatusOK, gin.H{"result": "ok", "data": user})
 		}
 	} else {
 		c.JSON(http.StatusInternalServerError, gin.H{"status": "Unable to bind data"})
