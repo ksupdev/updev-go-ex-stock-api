c448e7930f84cf0eb94a4487819fc2eff48fc6b0
Karoon Sillapapan <karoon@ananda.co.th>
2021-05-23 15:36:18 +0700

    Call database setup method

--- api/api.go
+++ api/api.go
@@ -2,9 +2,13 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/ksupdev/updev-go-ex-stock-api/db"
 )
 
 func Setup(router *gin.Engine) {
+
+	db.SetupDB()
+
 	SetupAuthenAPI(router)
 	SetupProductAPI(router)
 	SetupTransactionAPI(router)
