3d97afb0c66c85104dfe0bc3396e7707e9f3639e
Karoon Sillapapan <karoon@ananda.co.th>
2021-05-23 14:08:07 +0700

    Setup product route

--- api/product.api.go
+++ api/product.api.go
@@ -1,7 +1,33 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SetupProductAPI(router *gin.Engine) {
+	productAPI := router.Group("/api/v2")
+	{
+		productAPI.GET("/product", getProduct)
+		productAPI.GET("/product/:id", getProductByID)
+		productAPI.POST("/product", createProduct)
+		productAPI.PUT("/product", editProduct)
+	}
+}
 
+func getProduct(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"result": "get product"})
+}
+
+func getProductByID(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"result": "get product by ID"})
+}
+
+func createProduct(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"result": "create product"})
+}
+
+func editProduct(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"result": "edit product"})
 }
