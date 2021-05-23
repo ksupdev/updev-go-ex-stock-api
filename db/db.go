3c9a62e461cf8085c00e7e83bee70e9bf81b42f7
Karoon Sillapapan <karoon@ananda.co.th>
2021-05-23 15:29:34 +0700

    Config database connection

---
+++ db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/ksupdev/updev-go-ex-stock-api/model"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func GetDB() *gorm.DB {
+	return db
+}
+
+func SetupDB() {
+	database, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("Faild to connect database")
+	}
+
+	database.AutoMigrate(&model.User{})
+	database.AutoMigrate(&model.Product{})
+	database.AutoMigrate(&model.Transaction{})
+
+	db = database
+
+}
