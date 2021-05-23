c3b95f7046635681f153ce450ecefb8cc6d7d056
Karoon Sillapapan <karoon@ananda.co.th>
2021-05-23 16:02:54 +0700

    create HashPassword and CheckPasswordHash func

--- api/authen.api.go
+++ api/authen.api.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/ksupdev/updev-go-ex-stock-api/model"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func SetupAuthenAPI(router *gin.Engine) {
@@ -25,5 +26,14 @@ func register(c *gin.Context) {
 	if c.ShouldBind(&user) == nil {
 		c.JSON(http.StatusOK, gin.H{"result": "register", "data": user})
 	}
+}
 
+func hashPassword(password string) (string, error) {
+	byte, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(byte), err
+}
+
+func checkPasswordHash(password, hash string) bool {
+	error := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return error == nil
 }
